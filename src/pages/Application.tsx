4a64f847f3d8cbd528cc68227b5d86bfa04eefc0
Yu Hsuan Tseng <cindy2270@gmail.com>
2025-11-17 21:09:07 +0100

    application page

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import { useParams } from "react-router-dom";
 import UpdateApplication from '../components/UpdateApplication';
+import CreateApplication from '../components/CreateApplication';
 
 const Application: React.FC = () => {
+    const { id } = useParams();
 
-    return (
-        <Container>
-            <UpdateApplication />
-        </Container>
-    );
+
+    if (id) {
+        return <UpdateApplication />;
+    }
+
+    return <CreateApplication />;
+
+    // return (
+    //     <Container>
+    //         <UpdateApplication />
+    //         <CreateApplication />
+    //     </Container>
+    // );
 };
 
 export default Application;
